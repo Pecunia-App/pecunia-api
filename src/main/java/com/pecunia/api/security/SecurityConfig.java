3f0f3574728d3286fb4a2b189a9c1d9974d2d17c
lenny-zanotelli <mysubscriptions@tuta.com>
2025-05-12 16:29:10 +0200

    chore(code-guidance): add editorconfig, checkstyle ; add javadoc and swagger config

---
+++ src/main/java/com/pecunia/api/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.pecunia.api.security;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+/**
+ * Security Configuration.
+ *
+ * @author torigon
+ * @version $Id: $Id
+ */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+  @Value("${cors.allowed-origin}")
+  private String allowedOrigin;
+
+  /**
+   * Security configuration.
+   * 
+   * @return http
+   * @throws Exception when security concern exists.
+   */
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(AbstractHttpConfigurer::disable).cors(cors -> cors.configurationSource(request -> {
+      CorsConfiguration config = new CorsConfiguration();
+      config.setAllowedOrigins(List.of(allowedOrigin));
+      config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+      config.setAllowedHeaders(List.of("*"));
+      config.setAllowCredentials(true);
+      return config;
+    })).authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).sessionManagement(
+        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    return http.build();
+  }
+
+
+}
