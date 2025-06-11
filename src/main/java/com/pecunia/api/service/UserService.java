06e721e50216482ed3a9bae14a85d4c8590af61c
Lunardo <thomaslunardo@gmail.com>
2025-06-11 16:08:00 +0200

    feat(User-service): add user service then implement the password encoder from security confi

---
+++ src/main/java/com/pecunia/api/service/UserService.java
@@ -0,0 +1,35 @@
+package com.pecunia.api.service;
+
+import com.pecunia.api.model.User;
+import com.pecunia.api.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+
+  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  public User registerUser(String email, String password, Set<String> roles) {
+    if(userRepository.existsByEmail(email)) {
+      throw new RuntimeException("Cet email est déjà utilisé");
+    }
+
+    User user = new User();
+    user.setEmail(email);
+    user.setPassword(passwordEncoder.encode(password));
+    user.setRoles(roles);
+
+    return userRepository.save(user);
+
+  }
+}
