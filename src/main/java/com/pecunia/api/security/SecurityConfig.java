a1eb56c8462b35995ebe6c291d18e4dd61759b06
Lunardo <thomaslunardo@gmail.com>
2025-06-11 16:08:00 +0200

    feat(User-service): add user service then implement the password encoder from security confi

--- src/main/java/com/pecunia/api/security/SecurityConfig.java
+++ src/main/java/com/pecunia/api/security/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -46,5 +48,8 @@ public class SecurityConfig {
     return http.build();
   }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+      return new BCryptPasswordEncoder();
+    }
 }
