ececc0185abf2de2cddfc4273e4df120616ab685
Lunardo <thomaslunardo@gmail.com>
2025-06-12 16:48:23 +0200

    feat: add User class, controller, mapper. Also add the authentication and jwt services

--- src/main/java/com/pecunia/api/security/SecurityConfig.java
+++ src/main/java/com/pecunia/api/security/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.pecunia.api.security;
 
 import java.util.List;
+
+import com.pecunia.api.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 /**
@@ -24,6 +29,14 @@ import org.springframework.web.cors.CorsConfiguration;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final CustomUserDetailsService customUserDetailsService;
+
+  public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomUserDetailsService customUserDetailsService) {
+    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    this.customUserDetailsService = customUserDetailsService;
+  }
 
   @Value("${cors.allowed-origin}")
   private String allowedOrigin;
@@ -43,9 +56,20 @@ public class SecurityConfig {
       config.setAllowedHeaders(List.of("*"));
       config.setAllowCredentials(true);
       return config;
-    })).authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).sessionManagement(
-        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    }))
+    .authorizeHttpRequests(auth -> auth.
+        anyRequest().permitAll()
+    )
+    .userDetailsService(customUserDetailsService)
+    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
   }
 
     @Bean
