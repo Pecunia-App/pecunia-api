ce7650d90cae514512f4a6507b595542301f514f
Lunardo <thomaslunardo@gmail.com>
2025-06-12 16:48:23 +0200

    feat: add User class, controller, mapper. Also add the authentication and jwt services

--- src/main/java/com/pecunia/api/service/UserService.java
+++ src/main/java/com/pecunia/api/service/UserService.java
@@ -1,35 +1,84 @@
 package com.pecunia.api.service;
 
+import com.pecunia.api.dto.UserDTO;
+import com.pecunia.api.mapper.UserMapper;
 import com.pecunia.api.model.User;
 import com.pecunia.api.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
-  private final UserRepository userRepository;
-  private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserMapper userMapper;
 
 
-  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-    this.userRepository = userRepository;
-    this.passwordEncoder = passwordEncoder;
-  }
-
-  public User registerUser(String email, String password, Set<String> roles) {
-    if(userRepository.existsByEmail(email)) {
-      throw new RuntimeException("Cet email est déjà utilisé");
+    public UserService(
+          UserRepository userRepository,
+          PasswordEncoder passwordEncoder,
+          UserMapper userMapper
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.userMapper = userMapper;
     }
 
-    User user = new User();
-    user.setEmail(email);
-    user.setPassword(passwordEncoder.encode(password));
-    user.setRoles(roles);
+    public User registerUser(String email, String password, Set<String> roles) {
+        if(userRepository.existsByEmail(email)) {
+            throw new RuntimeException("Cet email est déjà utilisé");
+        }
 
-    return userRepository.save(user);
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setRoles(roles);
 
-  }
+        return userRepository.save(user);
+    }
+
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(userMapper::convertToDTO).collect(Collectors.toList());
+    }
+    
+    public UserDTO getUserById(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return null;
+        }
+
+        return userMapper.convertToDTO(user);
+    }
+
+    public UserDTO updateUser(Long id, User userDetails) {
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return null;
+        }
+
+        user.setFirstname(userDetails.getFirstname());
+        user.setLastname(userDetails.getLastname());
+        user.setEmail(userDetails.getEmail());
+ 
+        User updatedUser = userRepository.save(user);
+        return userMapper.convertToDTO(updatedUser);
+    }
+
+    public boolean deleteUserById(Long id) {
+        User user = userRepository.findById(id).orElse(null);
+
+        if (user == null) {
+            return false;
+        }
+
+        userRepository.delete(user);
+        return true;
+    }
 }
