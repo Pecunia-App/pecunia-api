90e046077f752601158ef38badb472efcfe93bac
Lunardo <thomaslunardo@gmail.com>
2025-06-13 09:52:46 +0200

    fix(editor.config): back to the original config

--- src/main/java/com/pecunia/api/dto/UserRegistrationDTO.java
+++ src/main/java/com/pecunia/api/dto/UserRegistrationDTO.java
@@ -2,23 +2,23 @@ package com.pecunia.api.dto;
 
 public class UserRegistrationDTO {
 
-  private String email;
+    private String email;
 
-  private String password;
+    private String password;
 
-  public String getEmail() {
+    public String getEmail() {
     return email;
   }
 
-  public void setEmail(String email) {
+    public void setEmail(String email) {
     this.email = email;
   }
 
-  public String getPassword() {
+    public String getPassword() {
     return password;
   }
 
-  public void setPassword(String password) {
+    public void setPassword(String password) {
     this.password = password;
   }
 }
