3ebae35b0c7c41218ec9e7e95352252b69b34752
Lunardo <thomaslunardo@gmail.com>
2025-07-09 21:37:32 +0200

    feat(user-service): improve the update user: create a user update dto for better control

--- src/main/java/com/pecunia/api/controller/UserController.java
+++ src/main/java/com/pecunia/api/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.pecunia.api.controller;
 
 import com.pecunia.api.dto.UserDTO;
-import com.pecunia.api.model.User;
+import com.pecunia.api.dto.UserUpdateDTO;
 import com.pecunia.api.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -13,75 +13,51 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    
-    private final UserService userService;
 
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+  private final UserService userService;
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    @GetMapping()
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        List<UserDTO> users = userService.getAllUsers();
-        if (users.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
-        return ResponseEntity.ok(users);
-    }
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
+  @GetMapping()
+  public ResponseEntity<List<UserDTO>> getAllUsers() {
+    List<UserDTO> users = userService.getAllUsers();
+    return users.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(users);
+  }
 
-    @PreAuthorize("#id == authentication.principal.id or hasRole('ROLE_ADMIN')")
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
-        UserDTO user = userService.getUserById(id);
-        if (user == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(user);
-    }
+  @PreAuthorize("#id == authentication.principal.id or hasRole('ROLE_ADMIN')")
+  @GetMapping("/{id}")
+  public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+    UserDTO user = userService.getUserById(id);
+    return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+  }
 
   @PreAuthorize("hasRole('ROLE_ADMIN')")
   @GetMapping("/search-firstname")
   public ResponseEntity<List<UserDTO>> getUsersByFirstname(@RequestParam String searchTerms) {
     List<UserDTO> users = userService.getUserByFirstname(searchTerms);
-    if (users.isEmpty()) {
-      return ResponseEntity.noContent().build();
-    }
-    return ResponseEntity.ok(users);
+    return users.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(users);
   }
 
   @PreAuthorize("hasRole('ROLE_ADMIN')")
   @GetMapping("/search-lastname")
   public ResponseEntity<List<UserDTO>> getUsersByLastname(@RequestParam String searchTerms) {
     List<UserDTO> users = userService.getUserByLastname(searchTerms);
-    if (users.isEmpty()) {
-      return ResponseEntity.noContent().build();
-    }
-    return ResponseEntity.ok(users);
+    return users.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(users);
   }
 
-    @PreAuthorize("#id == authentication.principal.id or hasRole('ROLE_ADMIN')")
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @Valid @RequestBody User userDetails) {
-        UserDTO updatedUser = userService.updateUser(id, userDetails);
-        System.out.println("User details" + userDetails);
-        System.out.println("Update user" + updatedUser);
-        if (updatedUser == null) {
-            return ResponseEntity.notFound().build();
-        }
+  @PreAuthorize("#id == authentication.principal.id or hasRole('ROLE_ADMIN')")
+  @PutMapping("/{id}")
+  public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO userDetails) {
+    UserDTO updatedUser = userService.updateUser(id, userDetails);
+    return updatedUser == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(updatedUser);
+  }
 
-        return ResponseEntity.ok(updatedUser);
-    }
-
-    @PreAuthorize("#id == authentication.principal.id or hasRole('ROLE_ADMIN')")
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-      if (userService.deleteUserById(id)) {
-        return ResponseEntity.noContent().build();
-      } else {
-        return ResponseEntity.notFound().build();
-      }
-    }
+  @PreAuthorize("#id == authentication.principal.id or hasRole('ROLE_ADMIN')")
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+    return userService.deleteUserById(id) ? ResponseEntity.noContent().build() : ResponseEntity.notFound().build();
+  }
 }
