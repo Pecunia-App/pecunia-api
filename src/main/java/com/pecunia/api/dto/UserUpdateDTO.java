51673b8c3376c5a034989862fc72c7a9395c5b59
Lunardo <thomaslunardo@gmail.com>
2025-07-09 21:37:32 +0200

    feat(user-service): improve the update user: create a user update dto for better control

---
+++ src/main/java/com/pecunia/api/dto/UserUpdateDTO.java
@@ -0,0 +1,52 @@
+package com.pecunia.api.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class UserUpdateDTO {
+
+  @NotBlank(message = "Le prénom est obligatoire.")
+  private String firstname;
+
+  @NotBlank(message = "Le nom est obligatoire.")
+  private String lastname;
+
+  @NotBlank(message = "La photo est obligatoire.")
+  private String profilePicture;
+
+  @Email(message = "Email invalide.")
+  @NotBlank(message = "L'email est obligatoire.")
+  private String email;
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+
+  public String getProfilePicture() {
+    return profilePicture;
+  }
+
+  public void setProfilePicture(String profilePicture) {
+    this.profilePicture = profilePicture;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
