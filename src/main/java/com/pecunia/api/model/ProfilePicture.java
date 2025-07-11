48fafc842df0a3583fc4d4eaf1290330d6035678
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2025-07-11 11:50:15 +0200

    feat(Create-ProfilePicture,change-UserMapper,-User,-UserUpdateDTO,-UserService): creation of the profilepicture entity and add the relation one to one in the user entity and modify userupdatedto usermapper userservice to adapt the profilepicture

---
+++ src/main/java/com/pecunia/api/model/ProfilePicture.java
@@ -0,0 +1,44 @@
+package com.pecunia.api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ProfilePicture {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Lob
+  @Column(nullable = false, columnDefinition ="MEDIUMBLOB")
+  private byte[] picture;
+
+  @OneToOne(mappedBy = "profilePicture", cascade = CascadeType.ALL)
+  private User user;
+
+  // getter and setter
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public byte[] getPicture() {
+    return picture;
+  }
+
+  public void setPicture(byte[] picture) {
+    this.picture = picture;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
+}
