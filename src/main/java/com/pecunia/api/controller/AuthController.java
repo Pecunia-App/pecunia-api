af3d8e1c8611356748d423d907b1b7a2d6673e0b
Lunardo <thomaslunardo@gmail.com>
2025-07-11 12:00:00 +0200

    feat(user-and-auth-controllers): add tags on classes and methods to describe requests

--- src/main/java/com/pecunia/api/controller/AuthController.java
+++ src/main/java/com/pecunia/api/controller/AuthController.java
@@ -6,6 +6,8 @@ import com.pecunia.api.model.User;
 import com.pecunia.api.security.AuthenticationService;
 import com.pecunia.api.security.TokenBlackList;
 import com.pecunia.api.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Set;
 
 @RestController
+@Tag(name = "Auth controller", description = "Handle registration, login and logout")
 @RequestMapping("/auth")
 public class AuthController {
 
@@ -29,6 +32,7 @@ public class AuthController {
   }
 
   @PostMapping("/register")
+  @Operation(summary = "Register a new user", description = "Creates a new user account with the role 'ROLE_USER'. Requires firstname, lastname, email, and password.")
   public ResponseEntity<User> register(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO) {
     User registeredUser = userService.registerUser(
             userRegistrationDTO.getFirstname(),
@@ -42,6 +46,7 @@ public class AuthController {
   }
 
   @PreAuthorize("hasRole('ADMIN')")
+  @Operation(summary = "Create a new user", description = "Same as register method but for admin have better controle.")
   @PostMapping("/create-user")
   public ResponseEntity<User> createUser(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO) {
     User registeredUser = userService.registerUser(
@@ -55,6 +60,7 @@ public class AuthController {
     return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
   }
 
+    @Operation(summary = "Authenticate a user and return a JWT token", description = "Authenticates a user using email and password. Returns a JWT token if credentials are valid.")
     @PostMapping("/login")
     public ResponseEntity<String> authenticate(@RequestBody UserLoginDTO userLoginDTO) {
         String token = authenticationService.authenticate(
@@ -65,6 +71,7 @@ public class AuthController {
         return ResponseEntity.ok(token);
     }
 
+  @Operation(summary = "Logout a user", description = "Invalidates the JWT token by adding it to a blacklist. Requires Authorization header.")
   @PostMapping("/logout")
   public ResponseEntity<String> logout(@RequestHeader("Authorization") String authorizationHeader) {
     String token = authorizationHeader.substring(7);
