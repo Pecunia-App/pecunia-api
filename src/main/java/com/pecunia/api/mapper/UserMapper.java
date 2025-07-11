18c46d1accd33670cdfe76b33a9158b09fafec81
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2025-07-11 11:50:15 +0200

    feat(Create-ProfilePicture,change-UserMapper,-User,-UserUpdateDTO,-UserService): creation of the profilepicture entity and add the relation one to one in the user entity and modify userupdatedto usermapper userservice to adapt the profilepicture

--- src/main/java/com/pecunia/api/mapper/UserMapper.java
+++ src/main/java/com/pecunia/api/mapper/UserMapper.java
@@ -4,18 +4,24 @@ import com.pecunia.api.dto.UserDTO;
 import com.pecunia.api.model.User;
 import org.springframework.stereotype.Component;
 
+import java.util.Base64;
+
 @Component
 public class UserMapper {
 
   public UserDTO convertToDTO(User user) {
 
-    UserDTO userDTO = new UserDTO();
-    userDTO.setId(user.getId());
-    userDTO.setFirstname(user.getFirstname());
-    userDTO.setLastname(user.getLastname());
-    userDTO.setProfilePicture(user.getProfilePicture());
-    userDTO.setEmail(user.getEmail());
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(user.getId());
+        userDTO.setFirstname(user.getFirstname());
+        userDTO.setLastname(user.getLastname());
+        userDTO.setEmail(user.getEmail());
 
-    return userDTO;
-  }
+        if (user.getProfilePicture() != null && user.getProfilePicture().getPicture() != null) {
+          String base64Picture = Base64.getEncoder().encodeToString(user.getProfilePicture().getPicture());
+          userDTO.setProfilePicture(base64Picture);
+        }
+
+        return userDTO;
+    }
 }
