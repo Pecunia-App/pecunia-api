7bc78e69b72ed3c7ed732dc0aa55ad855514e903
Daniel Gonzalez <daniel.gonzalez.02@gmail.com>
2025-08-25 19:35:41 +0200

    feat(data-init): add test user in dev profile for frontend testing

---
+++ src/main/java/com/pecunia/api/config/DevDataInitializater.java
@@ -0,0 +1,39 @@
+package com.pecunia.api.config;
+
+import com.pecunia.api.model.User;
+import com.pecunia.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Profile("dev")
+public class DevDataInitializater {
+
+  @Value("${test.user.password}")
+  private String testUserPassword;
+
+  @Bean
+  public CommandLineRunner initUserTest(
+      UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    return args -> {
+      if (!userRepository.existsByEmail("test@pecunia.fr")) {
+        User user = new User();
+        user.setFirstname("Test");
+        user.setLastname("Pecunia");
+        user.setEmail("test@pecunia.fr");
+        user.setPassword(passwordEncoder.encode(testUserPassword));
+        user.getRoles().add("ROLE_USER");
+        userRepository.save(user);
+
+        System.out.println("Utilisateur de test créé : " + user.getEmail());
+      } else {
+        User existingUser = userRepository.findByEmail("test@pecunia.fr").get();
+        System.out.println("L'utilisateur de test existe déjà : " + existingUser.getEmail());
+      }
+    };
+  }
+}
