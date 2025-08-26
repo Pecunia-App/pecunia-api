e12b183f97bccaf89f5c1cc06be9ab97202b3d72
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2025-08-26 17:37:13 +0200

    test(ProfilePictureControllerTest,-ProfilePictureDTO,-ProfilePictureMapper,-ProfilePictureServiceTest): add the test for the controller part and change the received version of the image

--- src/main/java/com/pecunia/api/mapper/ProfilePictureMapper.java
+++ src/main/java/com/pecunia/api/mapper/ProfilePictureMapper.java
@@ -4,20 +4,28 @@ import com.pecunia.api.dto.ProfilePictureDTO;
 import com.pecunia.api.model.ProfilePicture;
 import org.springframework.stereotype.Component;
 
+import java.util.Base64;
+
 
 @Component
 public class ProfilePictureMapper {
   public ProfilePicture convertToEntity(ProfilePictureDTO profilePictureDTO) {
     ProfilePicture profilePicture = new ProfilePicture();
-    profilePicture.setPicture(profilePictureDTO.getPicture());
 
+    if (profilePictureDTO.getPicture() != null) {
+      profilePicture.setPicture(Base64.getDecoder().decode(profilePictureDTO.getPicture()));
+    }
     return profilePicture;
   }
 
   public ProfilePictureDTO convertToDTO(ProfilePicture profilePicture) {
     ProfilePictureDTO profilePictureDTO = new ProfilePictureDTO();
     profilePictureDTO.setId(profilePicture.getId());
-    profilePictureDTO.setPicture(profilePicture.getPicture());
+
+    if (profilePicture.getPicture() != null) {
+      // encoder en Base64 pour exposer côté API
+      profilePictureDTO.setPicture(Base64.getEncoder().encodeToString(profilePicture.getPicture()));
+    }
     if (profilePicture.getUser() != null) {
       profilePictureDTO.setUserId(profilePicture.getUser().getId());
     }
