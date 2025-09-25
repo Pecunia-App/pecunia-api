9692af3b59df463e9fcf11c511bb09b7781743c4
AlexiaGu <150676364+AlexiaGu@users.noreply.github.com>
2025-09-25 13:04:18 +0200

    fix(modify-UserController,-PasswordUpdateDTO-and-UserService): corrections after repo comments and bug updatePassword

--- src/main/java/com/pecunia/api/dto/PasswordUpdateDTO.java
+++ src/main/java/com/pecunia/api/dto/PasswordUpdateDTO.java
@@ -1,17 +1,22 @@
 package com.pecunia.api.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class PasswordUpdateDTO {
 
+  @JsonProperty("newPassword")
   @NotBlank(message = "Le nouveau mot de passe est obligatoire.")
   @Size(min = 12, max = 128, message = "Le mot de passe doit contenir entre 12 et 128 caractères.")
   private String newPassword;
 
+  @JsonProperty("confirmNewPassword")
   @NotBlank(message = "La confirmation du mot de passe est obligatoire.")
   private String confirmNewPassword;
+
+  public PasswordUpdateDTO() {}
 
   // Vérifie que les deux champs correspondent
   @AssertTrue(message = "Les mots de passe ne correspondent pas.")
